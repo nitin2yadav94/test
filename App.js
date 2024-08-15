e584db3dc099f26128446a0320aea98359144c42
Nitin Yadav <nitin2yadav94@gmail.com>
2024-08-15 21:37:02 +0530

    funcitonal comp

--- App.js
+++ App.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement('div', null, [
-    React.createElement('div', {id: "child1"}, [
-        React.createElement('h1', null, 'Child 1 1 heading 1'),
-        React.createElement('h2', null, 'Child 1 heading 2')
-    ]),
-    React.createElement('div', {id: "child2"}, [
-        React.createElement('h1', null, 'Child 2 heading 1'),
-        React.createElement('h2', null, 'Child 2 heading 2')
-    ])
-]);
+
+// JSX - "JavaScript XML", html like structure in JavaScript
+// Babel - "JavaScript Compiler"
+// typeScript - "TypeScript"
+// Parcel - "Bundler" , babel 
+const heading1 = <h1>hello</h1>
+
+//Anonymous function,
+//arrow vs normal function
+//const & let & var
+
+//component - simple Javascript fucntion returning any JSX
+
+//Class Component - stateFull compoent
+//Functional component - stateLess compoennt
+//react >18 - React Hooks then fucntion component can have state
+
+const App = ()=>{
+    const s1 = 0;
+    return (<div>
+        <h1>hello</h1>
+    </div>)
+}
+
+const App1 = ()=> (
+<div><h1>hello</h1> 
+<h2>hello</h2>
+<h3>hello</h3>
+</div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(App1);
