15d711ff0707d3056a611e10f8c71839df13ce58
Nitin Yadav <nitin2yadav94@gmail.com>
2024-08-15 20:47:31 +0530

    commit

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 const heading = React.createElement('div', null, [
     React.createElement('div', {id: "child1"}, [
-        React.createElement('h1', null, 'Child 1 heading 1'),
+        React.createElement('h1', null, 'Child 1 1 heading 1'),
         React.createElement('h2', null, 'Child 1 heading 2')
     ]),
     React.createElement('div', {id: "child2"}, [
