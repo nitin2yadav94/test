f57531691c35f68147eefc2e3da30b2d35317043
Nitin Yadav <nitin2yadav94@gmail.com>
2024-08-15 20:37:42 +0530

    env file added

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement('h1', null, 'Hello World');
+const heading = React.createElement('div', null, [
+    React.createElement('div', {id: "child1"}, [
+        React.createElement('h1', null, 'Child 1 heading 1'),
+        React.createElement('h2', null, 'Child 1 heading 2')
+    ]),
+    React.createElement('div', {id: "child2"}, [
+        React.createElement('h1', null, 'Child 2 heading 1'),
+        React.createElement('h2', null, 'Child 2 heading 2')
+    ])
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
